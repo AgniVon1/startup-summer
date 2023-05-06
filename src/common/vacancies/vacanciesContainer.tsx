795346f1ae2820d8417285c4eedae5ce655fc2e0
AgniVon1 <iliatron00@gmail.com>
2023-05-06 03:24:26 +0300

    feat: vacanciesApi endpoint getVacanciesQuery

--- src/common/vacancies/vacanciesContainer.tsx
+++ src/common/vacancies/vacanciesContainer.tsx
@@ -3,11 +3,15 @@ import {vacanciesApi} from "./vacanciesApi";
 
 
 const VacanciesContainer = () => {
-  const {data,isLoading} = vacanciesApi.useFetchAllVacanciesQuery('')
-  console.log(data)
+  //const {data,isLoading} = vacanciesApi.useFetchAllVacanciesQuery('')
+  const {data,isLoading} = vacanciesApi.useGetVacanciesQuery({payment_from:10000, payment_to:100000,page: 0, count:3})
+
   return (
     <div>
       {!isLoading && data.objects[0].address}
+      {!isLoading && data.objects[1].address}
+      {!isLoading && data.objects[2].address}
+      {!isLoading || "....."}
     </div>
   );
 };
